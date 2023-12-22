57cdc31bd097a5bfea011a5131fa07280d1b8036
Milly <wmr4job@gmail.com>
2023-12-22 18:16:14 +0800

    read and write

---
+++ 20231222_read_and_write_file.py
@@ -0,0 +1,30 @@
+# read & write file
+
+products = []
+
+# 讀取檔案
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue # 繼續，跳過這一次的迴圈
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+# 輸入商品,價格清單
+while True:
+	name = input('請輸入商品名稱(輸入q離開)：')
+	if name == 'q': # 輸入q離開(quit)
+		break
+	price = input('請輸入商品價格：')
+	products.append([name, price]) # 存每個品項+價格
+
+# 印出商品價格
+for item in products:
+	print(item[0], '的價格是', item[1])
+
+# 寫入檔案
+with open('products.csv', 'w', encoding = 'utf-8') as f: # 打開檔案，並使用UTF-8的編碼
+	f.write('商品,價格\n') # 增加標題列
+	for item in products:
+		f.write(item[0] + ',' + item[1] + '\n') # f.write將內容寫入，加上','是為了分隔內容
